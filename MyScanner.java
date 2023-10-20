14f5a17896bb72ecee4e5f2f9407d37604075818
Sean Pohto <sean.pohto@mrjaffesclass.com>
2023-10-20 11:03:54 -0700

    commit message

--- MyScanner.java
+++ MyScanner.java
@@ -1,12 +1,21 @@
 import java.util.Scanner;
-
 public class MyScanner
 {
 
     public void run() {
         Scanner scanner = new Scanner(System.in);
+        Scanner n = new Scanner (System.in);
+        Scanner s = new Scanner (System.in).useDelimiter("\\s*,\\s*");
         while (true) {
-
+            int x = scanner.nextInt();
+            System.out.println(x);
+            scanner.close();
+            int num = n.nextInt();
+            int mult = num*3;
+            System.out.println(mult);
+            n.close();
+            String w = s.next();
+            System.out.println(w);
         }
     }
 
